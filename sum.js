2a93ff985f783cd26141c4e9a4789607bb5aade8
Phathumzi <2129411>
2023-04-22 13:26:57 +0200

    testing code coverage

--- sum.js
+++ sum.js
@@ -4,17 +4,8 @@ function sum(num1, num2) {
   
   module.exports = sum;
 
-function minus(num1,num2){
-    return num1 - num2;
+  function minus(num1, num2) {
+    return num1 + num2;
   }
-
-  module.exports= minus;
-
-function multiply(num1, num2){
-    return num1 * num2;
-  }
-
-  module.exports = multiply;
-
-
-
+  
+  module.exports = minus;
