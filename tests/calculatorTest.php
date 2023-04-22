ebd977515131a56024d59ff8f6565e7e66bc8bed
kagiso008 <69651224+kagiso008@users.noreply.github.com>
2023-04-22 20:33:03 +0200

    add anotations

--- tests/calculatorTest.php
+++ tests/calculatorTest.php
@@ -6,15 +6,17 @@ use App\Calculator;
 class calculatorTest extends PHPUnit\Framework\TestCase
 {
     /**
-     * @covers Calculator\BasicCalculator
+     * @covers App\Calculator
      */
     public function testAdd()
     {
         $this->assertEquals(Calculator::add(1, 2), 3.0);
     }
+    /**
+     * @covers App\Calculator
+     */
     public function testSubtract()
     {
-        $this->assertEquals(Calculator::subtract(4, 2), 2.0);
         $this->assertEquals(Calculator::subtract(3, 2), 1.0);
     }
 }
