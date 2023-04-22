635b95127e62f0d9a29685e6eab269450d19c91a
Phathumzi <2129411>
2023-04-22 13:10:23 +0200

    testing code coverage

--- sum.test.js
+++ sum.test.js
@@ -4,26 +4,28 @@ test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test("adds 1 + 2 to equal 3", () => {
+test("adds 8+8 to equal 16", () => {
     expect(sum(8, 8)).toBe(16);
-  });
+});
 
-  test("adds 1 + 2 to equal 3", () => {
+test("adds 9+2 to equal 11", () => {
     expect(sum(9, 2)).toBe(11);
-  });
+});
 
-  test("adds 1 + 2 to equal 3", () => {
-    expect(sum(-11, 2)).toBe(9);
-  });
+test("adds -11 + 2 to equal -9", () => {
+    expect(sum(-11, 2)).toBe(-9);
+});
 
-  test("adds 1 + 2 to equal 3", () => {
-    expect(sum(-991, -89)).toBe(4);
-  });
+test("subtracts 9 - 2 to equal 7", () => {
+    expect(sum(9, 2)).toBe(7);
+});
 
-  test("adds 1 + 2 to equal 3", () => {
+test("subtracts 80 - 10 to equal 70", () => {
     expect(sum(1, 7)).toBe(8);
-  });
+});
 
-  test("adds 1 + 2 to equal 3", () => {
-    expect(sum(3, 2)).toBe(5);
-  });
+test("subracts -90 - 10 to equal -80", () => {
+    expect(sum(-90, -10)).toBe(-100);
+});
+
+
