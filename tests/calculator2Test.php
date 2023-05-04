573a3607a596f2a3c304dde69edb7fb4aed5c83a
Phathumzi <2129411>
2023-05-04 21:28:15 +0200

    checking code cov

---
+++ tests/calculator2Test.php
@@ -0,0 +1,22 @@
+<?php
+require 'vendor/autoload.php';
+
+use App\Calculator2;
+
+class CalculatorTest extends PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers App\Calculator
+     */
+    public function testAdd()
+    {
+        $this->assertEquals(Calculator2::add(1, 2), 3.0);
+    }
+    /**
+     * @covers App\Calculator
+     */
+    public function testSubtract()
+    {
+        $this->assertEquals(Calculator2::subtract(3, 2), 1.0);
+    }
+}
