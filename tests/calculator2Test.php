c5437f7afc854c76329f7cfdfefd4ff56c7e3b23
Phathumzi <2129411>
2023-05-09 17:20:31 +0200

    changed to sum and minus

--- tests/calculator2Test.php
+++ tests/calculator2Test.php
@@ -8,11 +8,15 @@ class calculator2Test extends PHPUnit\Framework\TestCase
     /**
      * @covers App\Calculator
      */
-    public function testAdd()
+    public function testSum()
     {
         $this->assertEquals(Calculator2::add(1, 2), 3.0);
     }
     /**
      * @covers App\Calculator
      */
+    public function testMinus()
+    {
+        $this->assertEquals(Calculator2::subtract(3, 2), 1.0);
+    }
 }
